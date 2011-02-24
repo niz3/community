9d1e21d857fadba37b1c8897a63e816d4aa14965
niz3 <nils.nyberg1@gmail.com>
2011-02-24 15:39:16 +0100

    skit

--- settings.php
+++ settings.php
@@ -1,9 +1,9 @@
 <?
 $db=array(
-	"host" => "localhost",
-	"username" => "community",
-	"password" => "dUpCMNpstWDydAPa",
-	"database" => "community",
+	"host" => "community-114771.mysql.binero.se",
+	"username" => "114771_ry70418",
+	"password" => "axVaEhG4zE",
+	"database" => "114771-community",
 	"prefix" => "com_",
 );
 ?>
